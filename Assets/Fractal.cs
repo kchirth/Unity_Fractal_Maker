3ce8e019b61ba248872ed3d90e1e29edbf18d519
kch5153 <kch5153@gmail.com>
2018-01-10 12:39:21 -0500

    Added project files

---
+++ Assets/Fractal.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh[] meshes;
+	public Material material;
+	public int maxDepth;
+	public float childScale;
+	private int depth = 0;
+	public float spawnProbability;
+
+	private static Vector3[] childDirections = {
+		Vector3.up,
+		Vector3.right,
+		Vector3.left,
+		Vector3.forward,
+		Vector3.back
+	};
+
+	private static Quaternion[] childOrientations = {
+		Quaternion.identity,
+		Quaternion.Euler (0f, 0f, -90f),
+		Quaternion.Euler (0f, 0f, 90f),
+		Quaternion.Euler(90f,0f,0f),
+		Quaternion.Euler (-90f,0f,0f)
+	};
+
+	private void Start(){
+		gameObject.AddComponent<MeshFilter> ().mesh = meshes[Random.Range(0, meshes.Length)];
+		gameObject.AddComponent<MeshRenderer> ().material = material;
+		GetComponent<MeshRenderer> ().material.color = 
+			Color.Lerp (Color.white, Color.yellow, (float)depth / maxDepth);
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+	
+	private IEnumerator CreateChildren() {
+		for (int i = 0; i < childDirections.Length; i++) {
+			if(Random.value < spawnProbability){
+				yield return new WaitForSeconds (Random.Range (0.1f,0.5f));
+				new GameObject ("Fractal Child").AddComponent<Fractal> ().
+					Initialize (this, i);
+			}
+		}
+	}
+
+	private void Initialize(Fractal parent, int childIndex) {
+		meshes = parent.meshes;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		spawnProbability = parent.spawnProbability;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+		transform.localRotation = childOrientations[childIndex];
+	}
+
+}
